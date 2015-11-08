f5a5ec82823ea2a92f3f389e717d4623548d3424
Sam Willis <samueljohnwillis@gmail.com>
2015-11-08 23:04:55 +0000

    --no-amend

--- app/main.js
+++ app/main.js
@@ -4,12 +4,9 @@ import ReactDOM from 'react-dom';
 import withingsApi from 'withings-api';
 import jquery from 'jquery';
 
-// Get the date
+// Get the todays's date
 var today = new Date();
-
-// The Withings 'day' starts from the end of the last day I think, hence the +1
-var dd = today.getDate()-1;
-
+var dd = today.getDate();
 var mm = today.getMonth()+1; //January is 0!
 var yyyy = today.getFullYear();
 
@@ -21,10 +18,11 @@ if(mm<10) {
     mm='0'+mm
 }
 
-today = yyyy+'-'+mm+'-'+dd;
-console.log(today);
+var today = yyyy+'-'+mm+'-'+dd;
 
-// Generate the Withings API base url
+console.log('Today ' + today);
+
+// Get the activity log from Withings
 var activityUrl = withingsApi.generateUrl({
 	url: "http://wbsapi.withings.net/v2/measure",
 
@@ -40,9 +38,13 @@ var activityUrl = withingsApi.generateUrl({
 	access_token_secret: "9e7d5e43e2ee399c5988190cdfe67d45107b413887b152080def30184d466ec"
 });
 
+var stepsGoal = 12000;
+
 // Steps component
 var Steps = React.createClass({
+
   render: function() {
+
     return (
       <div className="result">
         <div className="label">
@@ -52,6 +54,10 @@ var Steps = React.createClass({
         <div className="value">
           {this.props.steps}
         </div>
+
+        <div>
+          {stepsGoal}
+        </div>
       </div>
     );
   }
@@ -60,6 +66,11 @@ var Steps = React.createClass({
 // Distance component
 var Distance = React.createClass({
   render: function() {
+
+    var floorDistance = Math.floor(this.props.distance);
+    var stepsInMeters = ((floorDistance) / (this.props.steps));
+    var distanceGoal = (Math.floor((stepsInMeters) * (stepsGoal)));
+
     return (
       <div className="result">
         <div className="label">
@@ -67,7 +78,11 @@ var Distance = React.createClass({
         </div>
 
         <div className="value">
-          <span>{this.props.distance}</span>
+          <span>{floorDistance}m</span>
+        </div>
+
+        <div>
+          <span>{distanceGoal}m</span>
         </div>
       </div>
     );
@@ -77,6 +92,10 @@ var Distance = React.createClass({
 // Calories component
 var Calories = React.createClass({
   render: function() {
+
+    var floorTotalCalories = Math.floor(this.props.totalCalories);
+    var floorCalories = Math.floor(this.props.calories);
+
     return (
       <div className="result">
         <div className="label">
@@ -84,20 +103,27 @@ var Calories = React.createClass({
         </div>
 
         <div className="value">
-          <span>{this.props.calories}</span>
+          <span>{floorCalories}</span>
+        </div>
+
+        <div>
+          <span>{floorTotalCalories}</span>
         </div>
       </div>
     );
   }
 });
 
+console.log(activityUrl)
+
 // Results container
 var ResultBox = React.createClass({
   getInitialState: function() {
     return {
-      steps: '',
-      distance: '',
-      calories: ''
+      steps: '0',
+      distance: '0',
+      calories: '0',
+      totalCalories: '0',
     };
   },
 
@@ -108,7 +134,8 @@ var ResultBox = React.createClass({
         this.setState({
           steps: urlResults.body.steps,
           distance: urlResults.body.distance,
-          calories: urlResults.body.calories
+          calories: urlResults.body.calories,
+          totalCalories: urlResults.body.totalcalories,
         });
       }
     }.bind(this));
@@ -118,8 +145,8 @@ var ResultBox = React.createClass({
     return (
       <div className="result-box">
         <Steps steps={this.state.steps} />
-        <Distance distance={this.state.distance} />
-        <Calories calories={this.state.calories} />
+        <Distance distance={this.state.distance} steps={this.state.steps} />
+        <Calories calories={this.state.calories} totalCalories={this.state.totalCalories}/>
       </div>
     );
   }
@@ -129,3 +156,26 @@ ReactDOM.render(
   <ResultBox source={activityUrl}/>,
   document.getElementById('content')
 );
+
+// var yesterdayNoon = new Date(yyyy, mm, dd).getTime() / 1000 - 43200;
+//
+// var now = Math.floor(Date.now() / 1000);
+//
+// // Get the sleep data from Withings
+// var sleepUrl = withingsApi.generateUrl({
+// 	url: "http://wbsapi.withings.net/v2/sleep",
+//
+// 	parameters: {
+// 		action: "getsummary",
+// 		userid: "8680483",
+//     // enddate: now,
+// 		// startdate: yesterdayNoon,
+// 	},
+//
+// 	consumer_key: "8536748c0d33794f647c6448f765b7682e4f820e49718153246be40bfbc683",
+// 	consumer_secret: "2889eb3c8e43ecc9d03207219ef2e6fb52ce49d3c7b08de37294f2d390fd",
+// 	access_token: "c288268c2d2318eb62fe23ffed11038e10f122885c66367e3b26e9c429",
+// 	access_token_secret: "9e7d5e43e2ee399c5988190cdfe67d45107b413887b152080def30184d466ec"
+// });
+//
+// console.log(sleepUrl);
